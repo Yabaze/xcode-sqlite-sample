33e2fd6827d314c26309957dca9ba13443746e1f
Thomas <cool.mirakle@gmail.com>
2018-11-27 21:57:14 +0530

    Initial Commit

---
+++ SaliteDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaliteDb
+//
+//  Created by Yabaze T on 27/11/18.
+//  Copyright © 2018 Yabaze T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
